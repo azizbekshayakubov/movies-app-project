48c1b0def56c149f6ff5efe661b573afe514015b
Azizbek Shoyoqubov <a.shoyoqubov@evos.uz>
2023-08-06 22:42:32 +0500

    init movi-app-project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerLeft">
+        <Link to="/">
+          <img
+            className="header_icon"
+            src="
+
+https://files.readme.io/29c6fee-blue_short.svg "
+          />
+        </Link>
+        <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+          <span>Top Rated</span>
+        </Link>
+        <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+          <span>Upcoming</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/movieList/movieList.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import "./movieList.css";
+import { useParams } from "react-router-dom";
+import Cards from "../card/card";
+
+const MovieList = () => {
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [type]);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=073fc8abaf5a1d219a0356d164958204&language=en-Us`,
+    ).then((res) => res.json().then((data) => setMovieList(data.results)));
+  };
+
+  return (
+    <div className="movie__list">
+      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+      <div className="list__cards">
+        {movieList.map((movie) => (
+          <Cards movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
